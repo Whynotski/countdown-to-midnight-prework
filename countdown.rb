4e6a96833b623c0cbee9fe230e19f4bc0bed7e03
Laura Adam <laurawynosky@gmail.com>
2018-05-30 20:57:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,5 @@
 #write your code here
 
-def countdown
-  x=10 
-  While x>0 
-  puts "#{x} second(s)!"
-  x-=l 
-end
-puts "Happy New Year"
-
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
